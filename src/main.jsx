b8313e4a5606ac0619d528fc9be01be6617e44b6
Aleksandr2905 <natalukha@ukr.net>
2024-05-06 22:03:33 +0300

    add global styles & pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GlobalStyles } from "./styles/GlobalStyles.js";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme.js";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <GlobalStyles />
+  </ThemeProvider>
+  // </React.StrictMode>
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  color: {
+    green: "#59B17A",
+    lightGreen: "rgba(89, 177, 122, 0.10)",
+    darkGreen: "#3F945F",
+    grey: "#93939A",
+    lightGrey: "#F7F8FA",
+    white: "#FFFFFF",
+    black: "#1D1E21",
+  },
+};
